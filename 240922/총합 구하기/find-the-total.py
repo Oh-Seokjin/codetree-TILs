fa5e3b5daa70d07829927a29f80dea8ffc1194bf
Seokjin Oh <5hseokjin@gmail.com>
2024-09-22 02:16:34 +0900

    Title: 총합 구하기, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240922/총합 구하기/find-the-total.py
+++ 240922/총합 구하기/find-the-total.py
@@ -1,7 +1,7 @@
 total_sum = 0
 a, b = map(int, input().split())
-for i in range(a, b+1):
-    if a%6 == 0 and a%8 != 0:
-        total_sum += a
+for elem in range(a, b+1):
+    if elem%6 == 0 and elem%8 != 0:
+        total_sum += elem
 
 print(total_sum)
