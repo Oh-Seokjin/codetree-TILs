f52e96a439518ec45bed58a88f24fc8fb9543ab6
Seokjin Oh <5hseokjin@gmail.com>
2024-09-22 23:53:25 +0900

    Title: 원소의 합이 0, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240922/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+num = [list(map(int, input().split())) for _  in range(4)]
+cnt = 0
+
+dict_ab = {}
+dict_cd = {}
+
+for i in range(n):
+    for j in range(n):
+        if num[0][i] + num[1][j] not in dict_ab:
+            dict_ab[num[0][i] + num[1][j]] = 1
+        else:
+            dict_ab[num[0][i] + num[1][j]] += 1
+
+for i in range(n):
+    for j in range(n):
+        if num[2][i] + num[3][j] not in dict_cd:
+            dict_cd[num[2][i] + num[3][j]] = 1
+        else:
+            dict_cd[num[2][i] + num[3][j]] += 1
+
+for ab in dict_ab.keys():
+    for cd in dict_cd.keys():
+        if ab + cd == 0:
+            cnt += dict_ab[ab]
+
+print(cnt)
