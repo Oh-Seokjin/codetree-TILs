5863c2fc61607035f231709f3c0a40da23bfcf0e
Seokjin Oh <5hseokjin@gmail.com>
2024-09-23 00:24:00 +0900

    Title: 중앙값 계산 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240923/중앙값 계산 2/get-median-2.py
+++ 240923/중앙값 계산 2/get-median-2.py
@@ -3,8 +3,9 @@ n = int(input())
 nums = list(map(int, input().split()))
 
 for i in range(len(nums)):
-    # if i==0:
-    #     print(nums[0], end=" ")
+    if i==0:
+        print(nums[0], end=" ")
+        break
 
     if i%2==0:
         print(nums[int(i//2)], end=" ")
