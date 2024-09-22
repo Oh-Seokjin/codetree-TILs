91969adef0982dd70ab7c6f31eff3539c1041ae6
Seokjin Oh <5hseokjin@gmail.com>
2024-09-23 03:02:36 +0900

    Title: 비오는 날, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240923/비오는 날/rainy-day.py
+++ 240923/비오는 날/rainy-day.py
@@ -6,9 +6,8 @@ for i in range(n):
     date, day, weather = input().split()
     if weather != "Rain":
         continue
-    p_y, p_m, p_d = map(int, near_date[0].split("-"))
-    c_y, c_m, c_d = map(int, date.split("-"))
-    if c_y <= p_y and c_m <= p_m and c_d <= p_d:
+
+    if near_date[0] >= date:
         near_date.pop()
         near_date.append(date)
         near_day.pop()
