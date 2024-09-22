86b7ceb32c61b4cf86e1a5669346313ab81b7f9c
Seokjin Oh <5hseokjin@gmail.com>
2024-09-22 14:54:39 +0900

    Title: 부분 문자열의 개수, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240922/부분 문자열의 개수/number-of-substrings.py
+++ 240922/부분 문자열의 개수/number-of-substrings.py
@@ -1,4 +1,10 @@
 full_text = input()
 partial_text = input()
 
-print(full_text.count(partial_text))
+cnt = 0
+
+for i in range(0, len(full_text)):
+    if full_text[i:i+2] == partial_text:
+        cnt +=1
+
+print(cnt)
