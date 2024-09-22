2256aa67163f31c499e7a06d60de2a9fb3a20824
Seokjin Oh <5hseokjin@gmail.com>
2024-09-23 00:26:57 +0900

    Title: 중앙값 계산 2, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240923/중앙값 계산 2/get-median-2.py
+++ 240923/중앙값 계산 2/get-median-2.py
@@ -3,9 +3,9 @@ n = int(input())
 nums = list(map(int, input().split()))
 
 for i in range(len(nums)):
-    if len(nums)==1:
-        print(nums[0])
-        break
+    # if len(nums)==1:
+    #     print(nums[0])
+    #     break
 
     if i%2==0:
         temp = sorted(nums[:i+1])
