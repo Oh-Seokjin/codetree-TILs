d514f2511e33154a89639f0115c1409e19d8bbdf
Seokjin Oh <5hseokjin@gmail.com>
2024-09-23 16:44:25 +0900

    Title: 밭의 높이를 고르게하기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240923/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240923/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -8,7 +8,7 @@ min_idx = 0
 answer = 0
 
 for i in range(0, len(field)-t+1):
-    cnt_flatten = abs(h*t - sum(field[i:i+t]))
+    cnt_flatten = sum([abs(h-elem) for elem in field[i:i+t]])
     if cnt_flatten < min_act:
         min_act = cnt_flatten
         min_idx = i
