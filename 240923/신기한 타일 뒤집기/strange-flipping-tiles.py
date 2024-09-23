69c9fc48f12a1d1132404ac8f92a66ea4db0f6ff
Seokjin Oh <5hseokjin@gmail.com>
2024-09-23 10:58:21 +0900

    Title: 신기한 타일 뒤집기, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240923/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 240923/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,31 +1,35 @@
 n = int(input())
 
 answer = {"L":0, "R":0}
+prev_direction = 0
 
-for _ in range(n):
+for i in range(n):
     cnt, direction = input().split()
     cnt = int(cnt)
-    
+
+    if i == 0:
+        if direction == "L":
+            prev_direction = 0
+            answer["L"] += cnt
+            continue
+        if direction == "R":
+            prev_direction = 1
+            answer["R"] += cnt
+            continue
+
     if direction == "R":
-        if "L" not in answer:
-            answer[direction] += cnt
-        else:
-            if answer["L"] > cnt:
-                answer["R"] = answer["R"] + cnt
-                answer["L"] = answer["L"] - cnt                
-            else:
-                answer["R"] = answer["R"] + cnt
-                answer["L"] = 0                
+        if prev_direction == 1:
+            cnt -= 1
+        answer["R"] = answer["R"] + cnt
+        answer["L"] = max(answer["L"] - cnt, 0)
+        prev_direction = 1
 
     if direction == "L":
-        if "R" not in answer:
-            answer[direction] += cnt
-        else:
-            if answer["R"] > cnt:
-                answer["L"] = answer["L"] + cnt
-                answer["R"] = answer["R"] - cnt
-            else:
-                answer["L"] = answer["L"] + cnt
-                answer["R"] = 0
+        if prev_direction == 0:
+            cnt -= 1
+        answer["L"] = answer["L"] + cnt
+        answer["R"] = max(answer["R"] - cnt, 0)
+        prev_direction = 0
+    # print(answer)
 
 print(answer["L"], answer["R"])
