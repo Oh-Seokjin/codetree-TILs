06f9257694d5b830c131e58b66f02aafb22bd529
Seokjin Oh <5hseokjin@gmail.com>
2024-09-23 16:37:13 +0900

    Title: 밭의 높이를 고르게하기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240923/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240923/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -3,9 +3,17 @@ n, h, t = map(int, input().split())
 field = list(map(int, input().split()))
 
 min_act = 9999999
-for i in range(0, len(field)-t):
+min_idx = 0
+
+answer = 0
+
+for i in range(0, len(field)-t+1):
     cnt_flatten = abs(h*t - sum(field[i:i+t]))
-    print(i, cnt_flatten)
     if cnt_flatten < min_act:
         min_act = cnt_flatten
-print(min_act)
+        min_idx = i
+
+for elem in field[min_idx:min_idx+t]:
+    answer += abs(elem-h)
+
+print(answer)
