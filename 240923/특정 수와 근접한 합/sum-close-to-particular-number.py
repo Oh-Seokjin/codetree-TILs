7157c60825eee31f48361161e99feda2e342fe89
Seokjin Oh <5hseokjin@gmail.com>
2024-09-23 15:03:10 +0900

    Title: 특정 수와 근접한 합, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240923/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240923/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -1,22 +1,51 @@
-def combination(arr, n):
-    result = []
-    if n == 0:
-        return [[]]
-    
-    for i in range(len(arr)):
-        elem = arr[i]
-        for rest in combination(arr[i+1:], n-1):
-            result.append([elem] + rest)
-    return result
-        
-
 n, s = map(int, input().split())
 nums = list(map(int, input().split()))
+total = sum(nums)
+min_diff = 999999
 
-min_diff = 0
+for i in range(n):
+    for j in range(i+1, n-1):
+        temp_total = total - nums[i] - nums[j]
+        if temp_total-s < min_diff:
+            min_diff = temp_total-s
 
-comb_nums = combination(nums, n-2)
-comb_nums = sorted([sum(elem) for elem in comb_nums])
+print(min_diff)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def combination(arr, n):
+#     result = []
+#     if n == 0:
+#         return [[]]
+    
+#     for i in range(len(arr)):
+#         elem = arr[i]
+#         for rest in combination(arr[i+1:], n-1):
+#             result.append([elem] + rest)
+#     return result
+        
+
+# n, s = map(int, input().split())
+# nums = list(map(int, input().split()))
+
+# min_diff = 0
+
+# comb_nums = combination(nums, n-2)
+# comb_nums = sorted([sum(elem) for elem in comb_nums])
 
 # for i in range(len(comb_nums)):
 #     if i == 0:
