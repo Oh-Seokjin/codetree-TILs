06c86158f24d95d7520494a3678079a1c68d0de9
Seokjin Oh <5hseokjin@gmail.com>
2024-09-23 17:25:44 +0900

    Title: 개발팀의 능력, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240923/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 240923/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -9,19 +9,21 @@ def combination(arr, n):
     return result   
 
 team = list(map(int, input().split()))
-combination_team = combination(team, 2)
-
+combination_team = combination([i for i in range(len(team))], 2)
+idx = [0, 1, 2, 3, 4]
 flag = 0
 diff = 9999
 
 for team_a in combination_team:
-    rest = [elem for elem in team if elem not in team_a]
+    rest = [elem for elem in idx if elem not in team_a]
     combination_b = combination(rest, 2)
     
     for team_b in combination_b:
         team_c = [elem for elem in rest if elem not in team_b]
-        
-        capa = [sum(team_a),sum(team_b), sum(team_c)]
+        # print(team_a, team_b, team_c)
+        capa = [sum([team[i] for i in team_a]),sum([team[i] for i in team_b]), sum([team[i] for i in team_c])]
+        # capa = [0, 0, 0]
+        # print(capa)
 
         if len(set(capa)) != 3:
             continue
@@ -30,7 +32,8 @@ for team_a in combination_team:
         if temp_diff < diff:
             diff = temp_diff
             flag = 1
-    
+            # print(capa, temp_diff)
+
 if flag:
     print(diff)
 else:
