ff34fcec3c3d1a2a01dd4733c348bdec728dad81
Seokjin Oh <5hseokjin@gmail.com>
2024-09-23 13:05:57 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240923/T를 초과하는 연속 부분 수열/subsequence-above-t.py
+++ 240923/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -2,11 +2,19 @@ n, t = map(int, input().split())
 
 nums = list(map(int, input().split()))
 
-nums = [elem if elem>t else 0 for elem in nums]
+nums = [elem if elem>t else "x" for elem in nums]
 
-if sum(nums) == 0:
-    print(0)
+x_split = [i for i, value in enumerate(nums) if value == "x"]
+
+max_len = 0
+
+if len(x_split) == 0:
+    print(len(nums))
+elif len(x_split) == 1:
+    print(max(len(nums[:x_split[0]]), len(nums[x_split[0]+1:])))
 else:
-    nums = "".join(map(str, nums))
-    nums = nums.split("0")
-    print(len(max(nums, key=lambda n: len(n))))
+    for i in range(len(x_split)-1):
+        cur_len = x_split[i+1]-1 - x_split[i]
+        if cur_len > max_len:
+            max_len = cur_len
+    print(max_len)
