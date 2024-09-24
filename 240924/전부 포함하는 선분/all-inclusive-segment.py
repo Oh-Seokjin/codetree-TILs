8fef81b66a2c441eae29ebaf2faf7978c5c64548
Seokjin Oh <5hseokjin@gmail.com>
2024-09-24 16:33:49 +0900

    Title: 전부 포함하는 선분, Time: 159ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240924/전부 포함하는 선분/all-inclusive-segment.py
+++ 240924/전부 포함하는 선분/all-inclusive-segment.py
@@ -1,12 +1,11 @@
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 #####
-# print(arr)
 
-min_len = 103
+min_len = 100
 
 for i in range(n):
-    board = [0 for _ in range(103)]
+    board = [0 for _ in range(100)]
     for j in range(n):
         if i == j:
             continue
@@ -15,5 +14,4 @@ for i in range(n):
             board[k] += 1
 
     min_len = min(min_len, len("".join(list(map(str, board))).strip("0")))
-
 print(min_len)
