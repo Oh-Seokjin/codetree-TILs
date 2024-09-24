ac66797ae85c3795ecfbc875cdce92bc6bc94818
Seokjin Oh <5hseokjin@gmail.com>
2024-09-24 10:10:37 +0900

    Title: 스승의 은혜 3, Time: 279ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240924/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240924/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -5,21 +5,18 @@ wish_list = []
 
 for _ in range(n):
     wish_list.append(list(map(int, input().split())))
-wish_list.sort(key=lambda x: x[0], reverse=True)
-# print(wish_list)
+
 for i in range(n):
+    wish_list[i][0] /= 2
+    sum_wish_list = sorted([sum(elem) for elem in wish_list])
+    
     paid = 0
     temp_max_s = 0
 
-    wish_list[i][0] /= 2
-    for wish in wish_list:
-        if paid <= b:
-            paid += sum(wish)
+    for wish in sum_wish_list:
+        if paid+wish <= b:
+            paid += wish
             temp_max_s += 1
-    # print(max_s, temp_max_s)
-    if max_s <= temp_max_s:
-        max_s = temp_max_s
-    else:
-        break        
+    max_s = max(max_s, temp_max_s)
 
 print(max_s)
