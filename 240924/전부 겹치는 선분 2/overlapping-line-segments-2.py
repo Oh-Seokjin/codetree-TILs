d15678c6398c90bcab31fcef2e37de8b186d3f73
Seokjin Oh <5hseokjin@gmail.com>
2024-09-24 12:29:58 +0900

    Title: 전부 겹치는 선분 2, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 240924/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 240924/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -1,11 +1,18 @@
 n = int(input())
 
-lines = [0 for _ in range(100)]
+lines = [0 for _ in range(105)]
 for _ in range(n):
     s, e = map(int, input().split())
-    for i in range(s, e):
+    for i in range(s, e+1):
         lines[i] += 1
-if "".join(map(str, lines)).strip("0").count("0") <= 1:
+
+cnt = 0
+
+for line in lines:
+    if line == n-1:
+        cnt += 1
+
+if cnt:
     print("Yes")
 else:
     print("No")
