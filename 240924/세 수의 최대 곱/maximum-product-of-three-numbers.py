460214281bdea28198bbd3d00a75c1cc5824b02f
Seokjin Oh <5hseokjin@gmail.com>
2024-09-24 15:30:58 +0900

    Title: 세 수의 최대 곱, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240924/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 240924/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -6,10 +6,7 @@ max_mul = 0
 
 arr.sort()
 
-n2_p1 = 1
-n2_p1 *= arr[0]*arr[1]*arr[-1]
-
-p3 = 1
-p3 *= arr[-1]*arr[-2]*arr[-3]
+n2_p1 = arr[0]*arr[1]*arr[-1]
+p3 = arr[-1]*arr[-2]*arr[-3]
 
 print(max([max_mul, n2_p1, p3]))
