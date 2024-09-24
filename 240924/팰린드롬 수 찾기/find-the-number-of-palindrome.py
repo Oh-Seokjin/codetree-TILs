35e966836072ce12cea207807f185e93cbb29eea
Seokjin Oh <5hseokjin@gmail.com>
2024-09-24 10:36:54 +0900

    Title: 팰린드롬 수 찾기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240924/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -0,0 +1,17 @@
+def palindrome(text):
+    if len(text)%2==0:
+        return text[:len(text)//2] == text[len(text)//2:]
+    else:
+        return text[:len(text)//2] == text[len(text)//2+1:]
+    # print(text[:len(text)//2], text[len(text)//2+1:])    
+    # return text[:len(text)//2] == text[len(text)//2+1:]
+
+x, y = map(int, input().split())
+
+cnt = 0
+
+for num in range(x, y+1):
+    if palindrome(str(num)):
+        cnt+=1
+
+print(cnt)
