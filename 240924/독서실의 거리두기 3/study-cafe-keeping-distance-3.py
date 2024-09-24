7879569c801a0c47af018f2af66ddabe6a4c73ca
Seokjin Oh <5hseokjin@gmail.com>
2024-09-24 14:49:58 +0900

    Title: 독서실의 거리두기 3, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240924/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 240924/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -6,7 +6,8 @@ seats = sorted(seats.split("1"), key=lambda x: len(x), reverse=True)
 seats.append(seats[0][:len(seats[0])//2])
 seats.append(seats[0][len(seats[0])//2+1:])
 del seats[0]
-seats = [elem for elem in seats if len(elem)>0]
+seats.remove("")
+seats.remove("")
 seats.sort()
-# print(seats)
+print(seats)
 print(len(seats[0])+1)
