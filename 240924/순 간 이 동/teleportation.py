ea5a34a07aaba6a259099d82477c099f6e69081b
Seokjin Oh <5hseokjin@gmail.com>
2024-09-24 13:16:51 +0900

    Title: 순 간 이 동, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240924/순 간 이 동/teleportation.py
+++ 240924/순 간 이 동/teleportation.py
@@ -3,5 +3,6 @@ a, b, x, y = map(int, input().split())
 s, e = min(a, b), max(a, b)
 t_s, t_e = min(x, y), max(x, y)
 
-walk_time = abs(s-t_s) + abs(e-t_e)
-print(walk_time)
+t_walk_time = abs(s-t_s) + abs(e-t_e)
+walk_time = e-s
+print(min(walk_time, t_walk_time)
