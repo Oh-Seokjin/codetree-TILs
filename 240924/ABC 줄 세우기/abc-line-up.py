3cf00cd914b4dab4f40fead6f63afb33b30f9948
Seokjin Oh <5hseokjin@gmail.com>
2024-09-24 15:21:53 +0900

    Title: ABC 줄 세우기, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240924/ABC 줄 세우기/abc-line-up.py
@@ -0,0 +1,16 @@
+n = int(input())
+arr = list(input().split())
+goal = [chr(ord("A") + i) for i in range(n)]
+
+cnt = 0
+
+for i in range(n):
+    answer = chr(ord("A") + i)
+    if arr[i] == answer:
+        continue
+    
+    idx = arr.index(answer)
+    arr.insert(i, arr.pop(idx))
+    cnt += idx-i
+
+print(cnt)
