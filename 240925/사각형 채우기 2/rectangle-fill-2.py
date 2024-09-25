d396af8c22db2623aaeca967ec6abb96ade35219
Seokjin Oh <5hseokjin@gmail.com>
2024-09-25 16:37:37 +0900

    Title: 사각형 채우기 2, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240925/사각형 채우기 2/rectangle-fill-2.py
+++ 240925/사각형 채우기 2/rectangle-fill-2.py
@@ -1,11 +1,12 @@
 n = int(input())
 #####
 
-dp = []
-dp.append(1)
-dp.append(1)
+MOD = 10007
+dp = [0] * (n+1)
+dp[0] = 1
+dp[1] = 1
 
 for i in range(2, n+1):
-    dp.append(dp[i-1] + dp[i-2] + dp[i-2])
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-2]
 
-print(dp[n]%10007)
+print(dp[n]%MOD)
