1bb8ae8e037c54503cf65e37e42b50f93dbf26a8
Seokjin Oh <5hseokjin@gmail.com>
2024-09-26 11:33:05 +0900

    Title: 정수 사각형 최소 합, Time: 172ms, Memory: 25MB, Status: Passed - CodeTree

--- 240926/정수 사각형 최소 합/minimum-sum-path-in-square.py
+++ 240926/정수 사각형 최소 합/minimum-sum-path-in-square.py
@@ -12,8 +12,7 @@ for x in range(-1, -1*n-1, -1):
 for y in range(0, n):
     dp[y][n-1]  = dp[y-1][n-1] + board[y][n-1]
     
-
-# # fill
+# fill
 for y in range(1, n):
     for x in range(-2, -1*n-1, -1):
         dp[y][x] = min(dp[y-1][x], dp[y][x+1]) + board[y][x]
