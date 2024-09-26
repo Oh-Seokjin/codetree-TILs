a364d4b1ef694ea59ee851addc9bb44b625eb931
Seokjin Oh <5hseokjin@gmail.com>
2024-09-26 10:39:49 +0900

    Title: 정수 사각형 최대 합, Time: 157ms, Memory: 25MB, Status: Passed - CodeTree

--- 240926/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 240926/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -13,15 +13,16 @@ for x in range(n):
 
 for y in range(n):
     temp_sum = 0
-    for i in range(y):
-        temp_sum += board[y][0]
+    for i in range(y+1):
+        temp_sum += board[i][0]
     max_sum[y][0] = temp_sum
 
 # fill
 for row in range(1, n):
     for col in range(1, n):
-        max_sum[row][col] = max(max_sum[row-1][col], max_sum[row][col-1]) + board[row][col]
+        max_sum[row][col] = max(max_sum[row-1][col]+ board[row][col], max_sum[row][col-1]+ board[row][col]) 
 
 max_sum[0][0] = board[0][0]
-
+# for row in max_sum:
+#     print(row)
 print(max(max_sum[n-1]))
