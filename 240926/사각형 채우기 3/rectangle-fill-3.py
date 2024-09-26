339dfb21af0bcb23653fdb0d0f04245fc8aa4eb2
Seokjin Oh <5hseokjin@gmail.com>
2024-09-26 09:55:41 +0900

    Title: 사각형 채우기 3, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240926/사각형 채우기 3/rectangle-fill-3.py
+++ 240926/사각형 채우기 3/rectangle-fill-3.py
@@ -6,6 +6,8 @@ dp = [-1] * (n+3)
 dp[0], dp[1], dp[2] = 0, 2, 7
 
 for i in range(3, n+1):
-    dp[i] = 3*dp[i-1] + dp[i-2] -1
+    dp[i] = dp[i - 1] * 2 + dp[i - 2] * 3
+    for j in range(i-3, -1, -1):
+        dp[i] += dp[j]*2
 
 print(dp[n]%MOD)
