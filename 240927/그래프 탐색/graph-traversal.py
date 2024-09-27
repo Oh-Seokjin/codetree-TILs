2a0418eb3fc8f2f299170adb660330b1a040bac6
Seokjin Oh <5hseokjin@gmail.com>
2024-09-27 14:09:26 +0900

    Title: 그래프 탐색, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240927/그래프 탐색/graph-traversal.py
+++ 240927/그래프 탐색/graph-traversal.py
@@ -1,25 +1,24 @@
 n, m = map(int, input().split())
 graph = [[0 for _ in range(n+1)] for _ in range(n+1)]
 visited = [0 for _ in range(n+1)]
+#####
+cnt = 0
+def dfs(v):
+    global cnt    
+    for curr_v in range(1, n+1):
+        if graph[v][curr_v] and not visited[curr_v]:
+            visited[v] = 1
+            cnt += 1
+            dfs(curr_v)
+
+
+
 
 for _ in range(m):
     s, e = map(int, input().split())
     graph[s-1][e-1], graph[e-1][s-1] = 1, 1
 
-#####
-
-cnt = 0
 visited[1] = 1
-
-def dfs(v):
-    global cnt    
-    for curr_v in range(n):
-        if graph[v][curr_v] and not visited[curr_v]:
-            visited[v] = 1
-            cnt += 1
-            dfs(curr_v)
-            
-
 dfs(1)
 
 print(cnt)
