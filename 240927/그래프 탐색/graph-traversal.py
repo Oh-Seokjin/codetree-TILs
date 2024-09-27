016f435d16062f755815929d40383727cce80409
Seokjin Oh <5hseokjin@gmail.com>
2024-09-27 11:36:32 +0900

    Title: 그래프 탐색, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240927/그래프 탐색/graph-traversal.py
+++ 240927/그래프 탐색/graph-traversal.py
@@ -1,6 +1,6 @@
 n, m = map(int, input().split())
 graph = [[0 for _ in range(n)] for _ in range(n)]
-visited = [0 for _ in range(n)]
+visited = [0 for _ in range(n+1)]
 
 for _ in range(m):
     s, e = map(int, input().split())
