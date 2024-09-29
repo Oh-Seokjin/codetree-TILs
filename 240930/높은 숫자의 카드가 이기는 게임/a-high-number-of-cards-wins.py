1f9fc83e1fdb38f052fe990e6789d27d30d3f30b
Seokjin Oh <5hseokjin@gmail.com>
2024-09-30 02:07:20 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 3507ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240930/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 240930/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -7,15 +7,14 @@ for _ in range(n):
     b.append(a.pop(a.index(card)))
 
 a.sort()
+b.sort()
 a_win_cnt = 0
 
-for i in range(n):
-    cur_b = b.pop(0)
-    for j in range(len(a)):
-        cur_a = a[j]
-        if cur_b < cur_a:
-            a.pop(j)
-            a_win_cnt += 1
-            break
+b_idx = 0
+for a_idx in range(n):
+
+    if b_idx < n and a[a_idx] > b[b_idx]:
+        a_win_cnt += 1
+        b_idx += 1
 
 print(a_win_cnt)
