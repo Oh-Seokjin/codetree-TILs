de44067da912a7815091406866b8ee0a4e591175
Seokjin Oh <5hseokjin@gmail.com>
2024-09-29 23:41:55 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 154ms, Memory: 31MB, Status: Passed - CodeTree

--- 240929/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 240929/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -2,29 +2,14 @@ n = int(input())
 p = list(map(int, input().split()))
 ###
 max_profit = 0
+min_price = p[0]
 
-for i in range(n-1):
-    future_max = max(p[i+1:])
-    temp_profit = future_max - p[i]
-    max_profit = max(max_profit, temp_profit)
+for i in range(n):
+    profit = p[i] - min_price
+
+    if profit > max_profit:
+        max_profit = profit
+    if min_price > p[i]:
+        min_price = p[i]
 
 print(max_profit)
-
-
-# n = int(input())
-# arr = list(map(int, input().split()))
-# #####
-
-# answer = -1001
-# temp_sum = 0
-
-# for i in range(n):
-#     if temp_sum < 0:
-#         temp_sum = arr[i]
-#     else:
-#         temp_sum += arr[i]
-#     # print("Temp:", temp_sum, end=" ")
-#     # print("answer: ", answer)
-#     answer = max(answer, temp_sum)
-
-# print(answer)
