112343748fd6efbe7ee5fd1912445ffac32e65ec
Seokjin Oh <5hseokjin@gmail.com>
2024-09-29 22:02:44 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240929/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+jewels = [list(map(int, input().split())) for _ in range(n)]
+###
+
+for i in range(n):
+    w, p = jewels[i][0], jewels[i][1]
+    jewels[i].append(p/w)
+
+jewels.sort(key=lambda x: x[2], reverse=True)
+
+bag_value = 0
+
+while True:
+    if jewels[0][0] <= m:
+        bag_value += jewels[0][1]
+        m -=jewels[0][0]
+        jewels.pop(0)
+    else:
+        bag_value += jewels[0][2]*m
+        break
+
+print(f"{bag_value:.3f}")
