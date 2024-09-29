aea4fa9ca580950da011a3628a70c5fcbf5b891c
Seokjin Oh <5hseokjin@gmail.com>
2024-09-29 22:11:04 +0900

    Title: 숫자 합치기, Time: 5030ms, Memory: 33MB, Status: Time Limit Exceed - CodeTree

--- 240929/숫자 합치기/merge-numbers.py
+++ 240929/숫자 합치기/merge-numbers.py
@@ -7,10 +7,16 @@ cost = 0
 while True:
     if len(arr) == 1:
         break
-    min1 = arr.pop(arr.index(min(arr)))
-    min2 = arr.pop(arr.index(min(arr)))
-    temp_cost = min1+min2
-    cost += temp_cost
-    arr.append(temp_cost)
+    arr.sort(reverse=True)
+    min1 = arr.pop()
+    min2 = arr.pop()
+    cost += min1+min2
+    arr.append(min1+min2)
+
+    # min1 = arr.pop(arr.index(min(arr)))
+    # min2 = arr.pop(arr.index(min(arr)))
+    # temp_cost = min1+min2
+    # cost += temp_cost
+    # arr.append(temp_cost)
 
 print(cost)
