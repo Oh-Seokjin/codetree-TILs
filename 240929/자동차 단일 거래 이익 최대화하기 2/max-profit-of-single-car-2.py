5eb680f119121bdd9f1e32342cedf92d18f61f44
Seokjin Oh <5hseokjin@gmail.com>
2024-09-29 23:34:04 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240929/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 240929/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -5,7 +5,7 @@ p = list(map(int, input().split()))
 max_profit = 0
 
 for i in range(n):
-    future_max = max(p[i+1])
+    future_max = max(p[i+1:])
     temp_profit = future_max - p[i]
     max_profit = max(max_profit, temp_profit)
 
