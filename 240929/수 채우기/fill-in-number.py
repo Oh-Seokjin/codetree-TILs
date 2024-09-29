1822f91449ae41116cbfdbf49515fd17b8dbd09a
Seokjin Oh <5hseokjin@gmail.com>
2024-09-29 23:24:57 +0900

    Title: 수 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240929/수 채우기/fill-in-number.py
+++ 240929/수 채우기/fill-in-number.py
@@ -2,14 +2,14 @@ n = int(input())
 INF = 1e11
 ###
 
-ans = MAX_NUM
+ans = INF
 
 for i in range(0, INF + 1):
     remainder = n - 5 * i
     if remainder >= 0 and remainder % 2 == 0:
         ans = min(ans, i + (remainder // 2))
     
-if ans == MAX_NUM:
+if ans == INF:
     ans = -1
 
 print(ans)
