7be6385f22b70db94195c51cdfb153f420103ddb
Seokjin Oh <5hseokjin@gmail.com>
2024-09-30 22:47:16 +0900

    Title: 최단 Run Length 인코딩, Time: 5048ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 240930/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 240930/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -4,29 +4,26 @@ run = [s for s in text]
 
 rle = ""
 
-if len(set(run)) == 1:
-    cnt = str(run.count(run[0]))
-    rle = run[0]+cnt
-else:
-    while True:
-        if run[0] != run[-1]:
-            break
-        temp = run[-1]
-        run = [temp] + run[:-1]
-    
-    search = run[0]
-    cnt = 1
-    for i in range(1, len(run)):
-        if run[i] == search:
-            cnt += 1
-        else:
-            rle = rle + search + str(cnt)
-            search = run[i]
-            cnt = 1
-        if i == len(run)-1:
-            rle = rle + search + str(cnt)
+# if len(set(run)) == 1:
+#     cnt = str(run.count(run[0]))
+#     rle = run[0]+cnt
+# else:
+while True:
+    if run[0] != run[-1]:
+        break
+    temp = run[-1]
+    run = [temp] + run[:-1]
 
-    # print(run)
-    # print(rle)
+search = run[0]
+cnt = 1
+for i in range(1, len(run)):
+    if run[i] == search:
+        cnt += 1
+    else:
+        rle = rle + search + str(cnt)
+        search = run[i]
+        cnt = 1
+    if i == len(run)-1:
+        rle = rle + search + str(cnt)
 
 print(len(rle))
