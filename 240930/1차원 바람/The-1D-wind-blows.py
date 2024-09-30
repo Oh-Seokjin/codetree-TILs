5d52669b0dcab94a9fe52277c667211b6876b8ae
Seokjin Oh <5hseokjin@gmail.com>
2024-09-30 21:00:31 +0900

    Title: 1차원 바람, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240930/1차원 바람/The-1D-wind-blows.py
@@ -0,0 +1,69 @@
+n, m, q = map(int, input().split())
+building = [list(map(int, input().split())) for _ in range(n)]
+winds = [list(input().split()) for _ in range(q)]
+for i in range(q):
+    winds[i][0] = int(winds[i][0])
+    if winds[i][1] == "L":
+        winds[i][1] = 0
+    elif winds[i][1] == "R":
+        winds[i][1] = 1
+###
+
+def wind(f, direction):
+    global building
+
+    # 왼쪽에서 부는 바람
+    if direction % 2 == 0:
+        temp = building[f-1][m-1]
+        building[f-1] = [temp] + building[f-1][:m-1] 
+    # 오른쪽에서 부는 바람
+    elif direction % 2 == 1:
+        temp = building[f-1][0]
+        building[f-1] = building[f-1][1:] + [temp]
+
+def up_propagate(f):
+    flag = 0
+    if f == 0:
+        return flag
+    c_floor, u_floor = building[f-1], building[f-2]
+    for c, u in zip(c_floor, u_floor):
+        if c == u:
+            flag = 1
+    return flag
+
+def down_propagate(f):
+    flag = 0
+    if f == n:
+        return flag
+    c_floor, d_floor = building[f-1], building[f]
+    for c, d in zip(c_floor, d_floor):
+        if c == d:
+            flag = 1
+    return flag
+        
+
+for floor, direction in winds:
+    wind(floor, direction)
+    up_floor = floor
+    down_floor = floor
+    up_direction = direction
+    down_direction = direction
+    while True:
+        if up_propagate(up_floor):
+            up_floor -= 1
+            up_direction += 1
+            wind(up_floor, up_direction)
+        else:
+            break
+    while True:
+        if down_propagate(down_floor):
+            down_floor += 1
+            down_direction += 1
+            wind(down_floor, down_direction)
+        else:
+            break
+    
+for row in building:
+    for elem in row:
+        print(elem, end=" ")
+    print()
