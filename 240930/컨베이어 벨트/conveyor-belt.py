c58b2ec475d6eec36ac266f0633d5a343ca6592f
Seokjin Oh <5hseokjin@gmail.com>
2024-09-30 17:58:24 +0900

    Title: 컨베이어 벨트, Time: 167ms, Memory: 25MB, Status: Passed - CodeTree

--- 240930/컨베이어 벨트/conveyor-belt.py
+++ 240930/컨베이어 벨트/conveyor-belt.py
@@ -1,13 +1,40 @@
 n, t = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(2)]
+u = list(map(int, input().split()))
+d = list(map(int, input().split()))
 ###
 t = t % (2*n)
 
 for _ in range(t):
-    arr[0].insert(0, arr[1].pop())
-    arr[1].insert(0, arr[0].pop())
+    temp = u[n-1]
 
-for row in arr:
-    for elem in row:
-        print(elem, end=" ")
-    print()
+    # 위 배열 뒤로 한칸씩 밀기
+    for i in range(n-1, 0, -1):
+        u[i] = u[i-1]
+    u[0] = d[n-1]
+
+    # 아래 배열 뒤로 한칸씩 밀기
+    for i in range(n-1, 0, -1):
+        d[i] = d[i-1]
+    d[0] = temp
+
+for elem in u:
+    print(elem, end=" ")
+print()
+
+for elem in d:
+    print(elem, end=" ")
+
+# 아래처럼 풀거나, flatten해서 풀 수 있지만, 아무래도 2차원 행렬 조작 공부 목적에는 부합하지 않는 것 같아 해설 보고 다시 작성
+# n, t = map(int, input().split())
+# arr = [list(map(int, input().split())) for _ in range(2)]
+# ###
+# t = t % (2*n)
+
+# for _ in range(t):
+#     arr[0].insert(0, arr[1].pop())
+#     arr[1].insert(0, arr[0].pop())
+
+# for row in arr:
+#     for elem in row:
+#         print(elem, end=" ")
+#     print()
