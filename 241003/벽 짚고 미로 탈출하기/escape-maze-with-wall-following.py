a10f48b24ab55a8c9afafb8bbbc0fd8d86515d1f
Seokjin Oh <5hseokjin@gmail.com>
2024-10-03 21:29:55 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 104ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241003/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -0,0 +1,51 @@
+n = int(input())
+r, c = map(int, input().split())
+r, c = r-1, c-1
+board = [[x for x in input()] for _ in range(n)]
+###
+
+visited = [[[0, 0, 0, 0]]*n for _ in range(n)]
+
+cnt = 0
+
+def in_range(x, y):
+    return 0<=x and x<n and 0<=y and y<n
+
+dxs, dys = [0, -1, 0, 1], [1, 0, -1, 0]
+wall_x, wall_y = [1, 0, -1, 0], [0, 1, 0, -1]
+
+d = 0
+
+while True:
+    nr, nc = r+dxs[d], c+dys[d]
+    if nr == n or nc == n or nr == -1 or nc == -1:
+        cnt += 1
+        break
+    if sum(visited[nr][nc]) == 4:
+        cnt = -1
+        break
+
+    if board[nr][nc] == ".":
+        if board[nr+wall_x[d]][nc+wall_y[d]] == "#":
+            r, c = nr, nc
+            cnt += 1
+            visited[r][c][d] += 1
+        elif board[nr+wall_x[d]][nc+wall_y[d]] == ".":
+            r, c = nr, nc
+            cnt += 1
+            visited[r][c][d] += 1
+            d = d-1
+            if d == -1:
+                d = 3
+            r, c = r+dxs[d], c+dys[d]
+            cnt += 1
+            visited[r][c][d] += 1
+    elif board[nr][nc] == "#":
+        visited[r][c][d] += 1
+        d +=1
+        if d == 4:
+            d = 0
+
+print(cnt)
+# for row in board:
+#     print(row)
