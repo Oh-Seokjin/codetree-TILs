6b3fdad2013ea7e37135f64e3fb079bf8acd3f92
Seokjin Oh <5hseokjin@gmail.com>
2024-10-03 14:53:13 +0900

    Title: 최적의 십자 모양 폭발, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241003/최적의 십자 모양 폭발/best-cross-shape-bomb.py
@@ -0,0 +1,55 @@
+n = int(input())
+original_board = [list(map(int, input().split())) for _ in range(n)]
+###
+
+def in_range(x, y):
+    return 0<=x and x<n and 0<=y and y<n
+
+def bomb(board, x, y, size):
+    #가로
+    for ny in range(y-size, y+size+1):
+        if in_range(x, ny):
+            board[x][ny] = 0
+
+    #세로
+    for nx in range(x-size, x+size+1):
+        if in_range(nx, y):
+            board[nx][y] = 0
+    return board
+
+def gravity(board):
+
+    temp = [[]*n for _ in range(n)]
+    trans = list(map(list, zip(*board)))
+
+    for i in range(n):
+        for elem in trans[i]:
+            if elem != 0:
+                temp[i].append(elem)
+        for _ in range(n-len(temp[i])):
+            temp[i].insert(0, 0)
+    
+    trans = list(map(list, zip(*temp)))
+    return trans
+
+def count_pair(board):
+    cnt = 0
+    for x in range(n):
+        for y in range(n):
+            for dx, dy in zip([1, 0], [0, 1]):
+                nx, ny = x+dx, y+dy
+                if in_range(nx, ny) and board[nx][ny] and (board[x][y] == board[nx][ny]):
+                    cnt += 1
+    return cnt
+
+max_cnt = 0
+
+for bomb_size in range(n):
+    for i in range(n):
+        for j in range(n):
+            temp = [[original_board[x][y] for x in range(n)] for y in range(n)]
+            bomb(temp, i, j, bomb_size)
+            gravity(temp)
+            max_cnt = max(max_cnt, count_pair(temp))
+
+print(max_cnt)
