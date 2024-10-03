9c404d06e139903942d151eda55305e184c69ada
Seokjin Oh <5hseokjin@gmail.com>
2024-10-03 16:51:30 +0900

    Title: 떨어지는 1자 블록, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

--- 241003/떨어지는 1자 블록/falling-horizontal-block.py
+++ 241003/떨어지는 1자 블록/falling-horizontal-block.py
@@ -3,8 +3,13 @@ board = [list(map(int, input().split())) for _ in range(n)]
 k = k-1
 ###
 
-for i in range(n-1, -1, -1):
-    if board[i][k:k+m] == [0]*m:
+for i in range(n):
+    if i == n-1:
+        for j in range(m):
+            board[i][k+j] = 1
+        break
+    
+    if 1 in board[i+1][k:k+m]:
         for j in range(m):
             board[i][k+j] = 1
         break
