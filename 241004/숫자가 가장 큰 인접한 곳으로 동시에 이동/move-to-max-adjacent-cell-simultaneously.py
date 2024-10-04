0a2f5951dbfe6e6da432ddd3fd0b99effba3b8d3
Seokjin Oh <5hseokjin@gmail.com>
2024-10-04 13:17:17 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 133ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241004/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 241004/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -11,36 +11,30 @@ def in_range(x, y):
 
 def move():
     global balls
-    temp = [[0 for _ in range(n)] for _ in range(n)]
 
     for x in range(n):
         for y in range(n):
             if balls[x][y]:
                 for dx, dy in zip(dxs, dys):
                     nx, ny = x+dx, y+dy
-                    if in_range(nx, ny) and board[x][y] < board[nx][ny] and balls[x][y]:
-                        # print(nx, ny)
-                        temp[nx][ny] += 1
+                    if in_range(nx, ny) and board[x][y] < board[nx][ny]:
+                        balls[x][y] -= 1
+                        balls[nx][ny] += 1
                         break
-                    else:
-                        temp[x][y] += 1
-    return temp
 
-def remove_collision(temp):
+def remove_collision():
     global balls
+
     for x in range(n):
         for y in range(n):
-            if temp[x][y] >= 2:
-                temp[x][y] = 0
-    balls = temp
-    
+            if balls[x][y] >= 2 or balls[x][y] <= 0:
+                balls[x][y] = 0
 
 dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
 
 for _ in range(t):
-    temp = move()
-    remove_collision(temp)
-    # print(balls)
+    move()
+    remove_collision()
 
 cnt = 0
 
